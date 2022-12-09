afafefd512cf13f9d43f667c5d973f241bd11eb6
han16935 <han1693516@gmail.com>
2022-12-09 11:34:09 +0900

    ✨ Add ExpenseItem Component and import to App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import ExpenseItem from "./Components/ExpenseItem";
+import "./App.css";
 
 function App() {
   return (
@@ -17,6 +19,7 @@ function App() {
         >
           Learn React
         </a>
+        <ExpenseItem></ExpenseItem>
       </header>
     </div>
   );
