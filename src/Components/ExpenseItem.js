3d3e2592c5950b807396e65ee7b0d4dea76a80a3
han16935 <han1693516@gmail.com>
2022-12-12 16:18:51 +0900

    🧑‍💻 First receive data using props

--- src/Components/ExpenseItem.js
+++ src/Components/ExpenseItem.js
@@ -1,17 +1,13 @@
 import React from "react";
 import "./ExpenseItem.css";
 
-function ExpenseItem() {
-  const expenseDate = new Date(2022, 11, 10);
-  const expenseTitle = "Car Insurance";
-  const expenseAmount = 294.68;
-
+function ExpenseItem({ date, title, amount }) {
   return (
     <div className="expense-item">
-      <div>{expenseDate.toISOString()}</div>
+      <div>{date.toISOString()}</div>
       <div className="expense-item__description">
-        <h2>{expenseTitle}</h2>
-        <div className="expense-item__price">${expenseAmount}</div>
+        <h2>{title}</h2>
+        <div className="expense-item__price">${amount}</div>
       </div>
     </div>
   );
