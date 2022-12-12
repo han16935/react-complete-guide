c2fa74cb237b101e0a7ac3ca1d5444f8176b2f5f
han16935 <han1693516@gmail.com>
2022-12-12 16:34:29 +0900

    🧑‍💻 add css to width & add js to normal js logic

--- src/Components/ExpenseItem.js
+++ src/Components/ExpenseItem.js
@@ -2,9 +2,16 @@ import React from "react";
 import "./ExpenseItem.css";
 
 function ExpenseItem({ date, title, amount }) {
+  const month = date.toLocaleString("en-US", { month: "long" });
+  const year = date.getFullYear();
+  const day = date.getDate();
   return (
     <div className="expense-item">
-      <div>{date.toISOString()}</div>
+      <div>
+        <div>{month}</div>
+        <div>{year}</div>
+        <div>{day}</div>
+      </div>
       <div className="expense-item__description">
         <h2>{title}</h2>
         <div className="expense-item__price">${amount}</div>
