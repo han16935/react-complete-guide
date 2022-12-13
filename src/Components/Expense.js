3500e9068f4094a859ba1081dc84b794dacf276f
han16935 <han1693516@gmail.com>
2022-12-13 16:34:38 +0900

    🧑‍💻 Add Card component & remove overlap css

--- src/Components/Expense.js
+++ src/Components/Expense.js
@@ -1,9 +1,11 @@
 import React from "react";
+import Card from "./Card";
 import ExpenseItem from "./ExpenseItem";
+import "./Expense.css";
 
 function Expense({ expenses }) {
   return (
-    <div>
+    <Card className="expense">
       <ExpenseItem
         date={expenses[0].date}
         title={expenses[0].title}
@@ -19,7 +21,13 @@ function Expense({ expenses }) {
         title={expenses[2].title}
         amount={expenses[2].amount}
       />
-    </div>
+
+      <ExpenseItem
+        date={expenses[3].date}
+        title={expenses[3].title}
+        amount={expenses[3].amount}
+      />
+    </Card>
   );
 }
 
