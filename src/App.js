76bf439f29eb2c46631e5b8c8a142823ac52b4d6
han16935 <han1693516@gmail.com>
2022-12-13 10:34:01 +0900

    🧑‍💻 Solve First Practice(Secetion 3)

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import logo from "./logo.svg";
-import ExpenseItem from "./Components/ExpenseItem";
+import Expense from "./Components/Expense";
 import "./App.css";
 
 function App() {
@@ -40,26 +40,7 @@ function App() {
         >
           Learn React
         </a>
-        <ExpenseItem
-          date={expenses[0].date}
-          title={expenses[0].title}
-          amount={expenses[0].amount}
-        ></ExpenseItem>
-        <ExpenseItem
-          date={expenses[1].date}
-          title={expenses[1].title}
-          amount={expenses[1].amount}
-        ></ExpenseItem>
-        <ExpenseItem
-          date={expenses[2].date}
-          title={expenses[2].title}
-          amount={expenses[2].amount}
-        ></ExpenseItem>
-        <ExpenseItem
-          date={expenses[3].date}
-          title={expenses[3].title}
-          amount={expenses[3].amount}
-        ></ExpenseItem>
+        <Expense expenses={expenses} />
       </header>
     </div>
   );
---
+++ src/Components/Expense.js
@@ -0,0 +1,26 @@
+import React from "react";
+import ExpenseItem from "./ExpenseItem";
+
+function Expense({ expenses }) {
+  return (
+    <div>
+      <ExpenseItem
+        date={expenses[0].date}
+        title={expenses[0].title}
+        amount={expenses[0].amount}
+      />
+      <ExpenseItem
+        date={expenses[1].date}
+        title={expenses[1].title}
+        amount={expenses[1].amount}
+      />
+      <ExpenseItem
+        date={expenses[2].date}
+        title={expenses[2].title}
+        amount={expenses[2].amount}
+      />
+    </div>
+  );
+}
+
+export default Expense;
