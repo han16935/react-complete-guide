cdf853f9d0047d585cf4322ae7af11ccc259a31e
han16935 <han1693516@gmail.com>
2022-12-19 17:38:16 +0900

    ✨ Add onchange(print value in input)

--- src/Components/NewExpense/ExpenseForm.js
+++ src/Components/NewExpense/ExpenseForm.js
@@ -2,11 +2,15 @@ import React from "react";
 import "./ExpenseForm.css";
 
 const ExpenseForm = () => {
+  const titleChangeHandler = (event) => {
+    const target = event.target;
+    console.log(target.value);
+  };
   return (
     <form className="new-expense__controls">
       <div className="new-expense__control">
         <label>Title</label>
-        <input type="text" />
+        <input type="text" onChange={titleChangeHandler} />
       </div>
 
       <div className="new-expense__control">
