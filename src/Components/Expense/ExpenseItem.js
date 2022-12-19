acbb3e7187d1a5a89cfd0eaa010e7b8c25a7c2bf
han16935 <han1693516@gmail.com>
2022-12-19 16:25:27 +0900

    🧑‍💻 First use State in React

--- src/Components/Expense/ExpenseItem.js
+++ src/Components/Expense/ExpenseItem.js
@@ -1,15 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 import "./ExpenseItem.css";
 
 function ExpenseItem({ date, title, amount }) {
-  const clickHandler = () => console.log(`Hello!`);
+  const [newTitle, setTitle] = useState(title);
+  const clickHandler = () => {
+    setTitle("updated!");
+    console.log(`Hello!`);
+  };
   return (
     <Card className="expense-item">
       <ExpenseDate date={date} />
       <div className="expense-item__description">
-        <h2>{title}</h2>
+        <h2>{newTitle}</h2>
         <div className="expense-item__price">${amount}</div>
       </div>
       <button onClick={clickHandler}>Change Title</button>
