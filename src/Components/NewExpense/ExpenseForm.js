a0059d7b7a88e47d91f3eab42234b73f96ab0cf4
han16935 <han1693516@gmail.com>
2022-12-20 11:21:54 +0900

    ✨ Add processing data into form tag

--- src/Components/NewExpense/ExpenseForm.js
+++ src/Components/NewExpense/ExpenseForm.js
@@ -19,8 +19,18 @@ const ExpenseForm = () => {
     const date = event.target.value;
     setEnteredDate(date);
   };
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    const expenseData = {
+      title: enteredTitle,
+      amount: enteredAmount,
+      date: new Date(enteredDate),
+    };
+    console.log(expenseData);
+  };
   return (
-    <form className="new-expense__controls">
+    <form className="new-expense__controls" onSubmit={submitHandler}>
       <div className="new-expense__control">
         <label>Title</label>
         <input type="text" onChange={titleChangeHandler} />
