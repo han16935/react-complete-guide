ff3abf35d0f091f77d952922a7b442f699197920
han16935 <han1693516@gmail.com>
2022-12-28 17:26:49 +0900

    🎨 Add li tag to semantic HTML

--- src/Components/Expense/ExpenseItem.js
+++ src/Components/Expense/ExpenseItem.js
@@ -10,14 +10,16 @@ function ExpenseItem({ date, title, amount }) {
     console.log(`Hello!`);
   };
   return (
-    <Card className="expense-item">
-      <ExpenseDate date={date} />
-      <div className="expense-item__description">
-        <h2>{newTitle}</h2>
-        <div className="expense-item__price">${amount}</div>
-      </div>
-      <button onClick={clickHandler}>Change Title</button>
-    </Card>
+    <li>
+      <Card className="expense-item">
+        <ExpenseDate date={date} />
+        <div className="expense-item__description">
+          <h2>{newTitle}</h2>
+          <div className="expense-item__price">${amount}</div>
+        </div>
+        <button onClick={clickHandler}>Change Title</button>
+      </Card>
+    </li>
   );
 }
 
